57d70d7ed57894b4b6bd28b65a9b371ed9ea943e
Arch-it-12 <41963542+Arch-it-12@users.noreply.github.com>
2022-08-25 22:51:17 -0400

    Corrected for absolute position, and added acceleration telemetry;  Also forgot to set the DPP

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/TeleOp/Tests/ArmVelTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/TeleOp/Tests/ArmVelTest.java
@@ -6,13 +6,23 @@ import com.arcrobotics.ftclib.hardware.motors.Motor;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
+import org.firstinspires.ftc.teamcode.Constants.Arm.ArmState;
+import org.firstinspires.ftc.teamcode.Constants.MotorConst;
+
 // TeleOp Used to tune the max velocity and acceleration of the arm motor
 
-@TeleOp(name = "Arm Motor Test", group = "Tests")
-public class ArmMotorTest extends OpMode {
+@TeleOp(name = "Arm Velocity Test", group = "Tests")
+public class ArmVelTest extends OpMode {
+
+    // Constants
+    private final double dpp = (MotorConst.ARM.getTPR() * MotorConst.ARM.getGearRatio()) / (2 * Math.PI);
 
     // Hardware
     private Motor armMotor;
+
+    private double getCorrectedPosition() {
+        return armMotor.getDistance() + ArmState.INITIAL.getVal();
+    }
 
     @Override
     public void init() {
@@ -25,19 +35,22 @@ public class ArmMotorTest extends OpMode {
         // Init stuff
         armMotor.setInverted(true);
         armMotor.setZeroPowerBehavior(Motor.ZeroPowerBehavior.BRAKE);
+        armMotor.setDistancePerPulse(dpp);
+
         armMotor.resetEncoder();
     }
 
     @Override
     public void loop() {
-        if (armMotor.getCurrentPosition() < Math.PI) {
+        if (getCorrectedPosition() < Math.PI) {
             armMotor.set(1.0);
         } else {
             armMotor.stopMotor();
         }
 
-        telemetry.addData("Position", "%f rad", armMotor.getDistance());
-        telemetry.addData("Velocity", "%f rad/s",armMotor.getRate());
+        telemetry.addData("Position", "%f rad", getCorrectedPosition());
+        telemetry.addData("Velocity", "%f rad/s", armMotor.getRate());
+        telemetry.addData("Acceleration", "%f rad/s/s", armMotor.encoder.getAcceleration() * dpp);
         telemetry.update();
     }
 }
